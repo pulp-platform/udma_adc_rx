856bc8209334a6a4b78716c7b6c12fd532f3071b
Florian Glaser <glaserf@iis.ee.ethz.ch>
2019-03-05 17:36:24 +0100

    Completed multi-channel support

--- rtl/udma_adc_rx_reg_if.sv
+++ rtl/udma_adc_rx_reg_if.sv
@@ -23,105 +23,114 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 // register map
-`define REG_RX_SADDR     5'b00000 //BASEADDR+0x00 
-`define REG_RX_SIZE      5'b00001 //BASEADDR+0x04
-`define REG_RX_CFG       5'b00010 //BASEADDR+0x08  
-`define REG_RX_INTCFG    5'b00011 //BASEADDR+0x0C  
+`define REG_RX_SADDR     3'b000 //BASEADDR+0x00
+`define REG_RX_SIZE      3'b001 //BASEADDR+0x04
+`define REG_RX_CFG       3'b010 //BASEADDR+0x08
+`define REG_RX_INTCFG    3'b011 //BASEADDR+0x0C
+`define REG_SINGLE_CH    3'b100 //BASEADDR+0x10
 
 module udma_adc_rx_reg_if #(
     parameter L2_AWIDTH_NOAL = 12,
-    parameter TRANS_SIZE     = 16
+    parameter TRANS_SIZE     = 16,
+    parameter ADC_NUM_CHS    = 8
 ) (
 	input  logic 	                  clk_i,
 	input  logic   	                  rstn_i,
 
 	input  logic               [31:0] cfg_data_i,
-	input  logic                [4:0] cfg_addr_i,
+	input  logic                [7:0] cfg_addr_i,
 	input  logic                      cfg_valid_i,
 	input  logic                      cfg_rwn_i,
     output logic               [31:0] cfg_data_o,
 	output logic                      cfg_ready_o,
 
-    output logic [L2_AWIDTH_NOAL-1:0] cfg_rx_startaddr_o,
-    output logic     [TRANS_SIZE-1:0] cfg_rx_size_o,
-    output logic                [1:0] cfg_rx_datasize_o,
-    output logic                      cfg_rx_continuous_o,
-    output logic                      cfg_rx_en_o,
-    output logic                      cfg_rx_clr_o,
-    input  logic                      cfg_rx_en_i,
-    input  logic                      cfg_rx_pending_i,
-    input  logic [L2_AWIDTH_NOAL-1:0] cfg_rx_curr_addr_i,
-    input  logic     [TRANS_SIZE-1:0] cfg_rx_bytes_left_i
+    output logic [ADC_NUM_CHS-1:0][L2_AWIDTH_NOAL-1:0] cfg_rx_startaddr_o,
+    output logic [ADC_NUM_CHS-1:0]    [TRANS_SIZE-1:0] cfg_rx_size_o,
+    output logic [ADC_NUM_CHS-1:0]               [1:0] cfg_rx_datasize_o,
+    output logic [ADC_NUM_CHS-1:0]                     cfg_rx_continuous_o,
+    output logic [ADC_NUM_CHS-1:0]                     cfg_rx_en_o,
+    output logic [ADC_NUM_CHS-1:0]                     cfg_rx_clr_o,
+    input  logic [ADC_NUM_CHS-1:0]                     cfg_rx_en_i,
+    input  logic [ADC_NUM_CHS-1:0]                     cfg_rx_pending_i,
+    input  logic [ADC_NUM_CHS-1:0][L2_AWIDTH_NOAL-1:0] cfg_rx_curr_addr_i,
+    input  logic [ADC_NUM_CHS-1:0]    [TRANS_SIZE-1:0] cfg_rx_bytes_left_i,
+
+    output logic cfg_single_ch_mode_o
 );
 
-    logic [L2_AWIDTH_NOAL-1:0] r_rx_startaddr;
-    logic   [TRANS_SIZE-1 : 0] r_rx_size;
-    logic              [1 : 0] r_rx_datasize;
-    logic                      r_rx_continuous;
-    logic                      r_rx_en;
-    logic                      r_rx_clr;
+    logic [ADC_NUM_CHS-1:0][L2_AWIDTH_NOAL-1:0] r_rx_startaddr;
+    logic [ADC_NUM_CHS-1:0]    [TRANS_SIZE-1:0] r_rx_size;
+    logic [ADC_NUM_CHS-1:0]                     r_rx_continuous;
+    logic [ADC_NUM_CHS-1:0]                     r_rx_en;
+    logic [ADC_NUM_CHS-1:0]                     r_rx_clr;
 
-    logic                [4:0] s_wr_addr;
-    logic                [4:0] s_rd_addr;
+    logic        r_single_ch_mode;
 
-    assign s_wr_addr = (cfg_valid_i & ~cfg_rwn_i) ? cfg_addr_i : 5'h0;
-    assign s_rd_addr = (cfg_valid_i &  cfg_rwn_i) ? cfg_addr_i : 5'h0;
 
-    assign cfg_rx_startaddr_o  = r_rx_startaddr;
-    assign cfg_rx_size_o       = r_rx_size;
-    assign cfg_rx_datasize_o   = r_rx_datasize;
-    assign cfg_rx_continuous_o = r_rx_continuous;
-    assign cfg_rx_en_o         = r_rx_en;
-    assign cfg_rx_clr_o        = r_rx_clr;
+    logic  [2:0] s_reg_sel_w, s_reg_sel_r;
+    logic  [4:0] s_ch_sel_w, s_ch_sel_r;
 
-    always_ff @(posedge clk_i, negedge rstn_i) 
-    begin
-        if(~rstn_i) 
-        begin
+    assign s_reg_sel_w = (cfg_valid_i & ~cfg_rwn_i) ? cfg_addr_i[2:0] : 'h0;
+    assign s_reg_sel_r = (cfg_valid_i &  cfg_rwn_i) ? cfg_addr_i[2:0] : 'h0;
+
+    assign s_ch_sel_w  = (cfg_valid_i & ~cfg_rwn_i) ? cfg_addr_i[7:3] : 'h0;
+    assign s_ch_sel_r  = (cfg_valid_i &  cfg_rwn_i) ? cfg_addr_i[7:3] : 'h0;
+
+    assign cfg_rx_startaddr_o   = r_rx_startaddr;
+    assign cfg_rx_size_o        = r_rx_size;
+    assign cfg_rx_datasize_o    = {ADC_NUM_CHS{2'b10}};
+    assign cfg_rx_continuous_o  = r_rx_continuous;
+    assign cfg_rx_en_o          = r_rx_en;
+    assign cfg_rx_clr_o         = r_rx_clr;
+    assign cfg_single_ch_mode_o = r_single_ch_mode;
+
+    always_ff @(posedge clk_i, negedge rstn_i) begin
+        if(~rstn_i) begin
             // SPI REGS
             r_rx_startaddr  <=  'h0;
             r_rx_size       <=  'h0;
             r_rx_continuous <=  'h0;
             r_rx_en          =  'h0;
             r_rx_clr         =  'h0;
-            r_rx_datasize   <= 2'b10;
-        end
-        else
-        begin
+            r_single_ch_mode = 1'b0;
+        end 
+        else begin
             r_rx_en          =  'h0;
             r_rx_clr         =  'h0;
 
-            if (cfg_valid_i & ~cfg_rwn_i)
-            begin
-                case (s_wr_addr)
-                `REG_RX_SADDR:
-                    r_rx_startaddr   <= cfg_data_i[L2_AWIDTH_NOAL-1:0];
-                `REG_RX_SIZE:
-                    r_rx_size        <= cfg_data_i[TRANS_SIZE-1:0];
-                `REG_RX_CFG:
-                begin
-                    r_rx_clr          = cfg_data_i[5];
-                    r_rx_en           = cfg_data_i[4];
-                    r_rx_datasize    <= cfg_data_i[2:1];
-                    r_rx_continuous  <= cfg_data_i[0];
-                end
-                endcase
+            if (cfg_valid_i & ~cfg_rwn_i) begin
+               case (s_reg_sel_w)
+                    `REG_RX_SADDR:
+                        r_rx_startaddr[s_ch_sel_w]   <= cfg_data_i[L2_AWIDTH_NOAL-1:0];
+                    `REG_RX_SIZE:
+                        r_rx_size[s_ch_sel_w]        <= cfg_data_i[TRANS_SIZE-1:0];
+                    `REG_RX_CFG:
+                    begin
+                        r_rx_clr[s_ch_sel_w]          = cfg_data_i[5];
+                        r_rx_en[s_ch_sel_w]           = cfg_data_i[4];
+                        r_rx_continuous[s_ch_sel_w]  <= cfg_data_i[0];
+                    end
+                    `REG_SINGLE_CH:
+                        r_single_ch_mode             <= cfg_data_i[0];
+               endcase
             end
         end
     end //always
 
-    always_comb
-    begin
+    always_comb begin
         cfg_data_o = '0;
-        case (s_rd_addr)
-        `REG_RX_SADDR:
-            cfg_data_o = cfg_rx_curr_addr_i;
-        `REG_RX_SIZE:
-            cfg_data_o[TRANS_SIZE-1:0] = cfg_rx_bytes_left_i;
-        `REG_RX_CFG:
-            cfg_data_o = {26'h0,cfg_rx_pending_i,cfg_rx_en_i,1'b0,r_rx_datasize,r_rx_continuous};
-        default:
-            cfg_data_o = '0;
+
+        case (s_reg_sel_r)
+            `REG_RX_SADDR:
+                cfg_data_o = cfg_rx_curr_addr_i[s_ch_sel_r];
+            `REG_RX_SIZE:
+                cfg_data_o[TRANS_SIZE-1:0] = cfg_rx_bytes_left_i[s_ch_sel_r];
+            `REG_RX_CFG:
+                cfg_data_o = {26'h0,cfg_rx_pending_i[s_ch_sel_r],cfg_rx_en_i[s_ch_sel_r],1'b0,2'b10,r_rx_continuous[s_ch_sel_r]};
+            `REG_SINGLE_CH:
+                cfg_data_o = {31'h0,r_single_ch_mode};
+            default:
+                cfg_data_o = '0;
         endcase
     end
 
--- rtl/udma_adc_rx_top.sv
+++ rtl/udma_adc_rx_top.sv
@@ -3,33 +3,36 @@
 module udma_adc_rx_top #(
   parameter L2_AWIDTH_NOAL = 12,
   parameter TRANS_SIZE     = 16,
-  parameter ADC_DATA_WIDTH = 32 )
+  parameter ADC_DATA_WIDTH = 32,
+  parameter ADC_NUM_CHS    = 8,
+  parameter CH_ID_LSB      = 28,
+  parameter CH_ID_WIDTH    = 4  )
 (
   input  logic                      sys_clk_i,      // master clock
   input  logic                      periph_clk_i,   // master clock
   input  logic                      rst_ni,         // asynchronous active low reset
 
   input  logic               [31:0] cfg_data_i,
-  input  logic                [4:0] cfg_addr_i,
+  input  logic                [7:0] cfg_addr_i,
   input  logic                      cfg_valid_i,
   input  logic                      cfg_rwn_i,
   output logic               [31:0] cfg_data_o,
   output logic                      cfg_ready_o,
 
-  output logic [L2_AWIDTH_NOAL-1:0] cfg_rx_startaddr_o,
-  output logic     [TRANS_SIZE-1:0] cfg_rx_size_o,
-  output logic                      cfg_rx_continuous_o,
-  output logic                      cfg_rx_en_o,
-  output logic                      cfg_rx_clr_o,
-  input  logic                      cfg_rx_en_i,
-  input  logic                      cfg_rx_pending_i,
-  input  logic [L2_AWIDTH_NOAL-1:0] cfg_rx_curr_addr_i,
-  input  logic     [TRANS_SIZE-1:0] cfg_rx_bytes_left_i,
+  output logic [ADC_NUM_CHS-1:0][L2_AWIDTH_NOAL-1:0] cfg_rx_startaddr_o,
+  output logic [ADC_NUM_CHS-1:0]    [TRANS_SIZE-1:0] cfg_rx_size_o,
+  output logic [ADC_NUM_CHS-1:0]                     cfg_rx_continuous_o,
+  output logic [ADC_NUM_CHS-1:0]                     cfg_rx_en_o,
+  output logic [ADC_NUM_CHS-1:0]                     cfg_rx_clr_o,
+  input  logic [ADC_NUM_CHS-1:0]                     cfg_rx_en_i,
+  input  logic [ADC_NUM_CHS-1:0]                     cfg_rx_pending_i,
+  input  logic [ADC_NUM_CHS-1:0][L2_AWIDTH_NOAL-1:0] cfg_rx_curr_addr_i,
+  input  logic [ADC_NUM_CHS-1:0]    [TRANS_SIZE-1:0] cfg_rx_bytes_left_i,
               
-  output logic                [1:0] data_rx_datasize_o,
-  output logic               [31:0] data_rx_o,
-  output logic                      data_rx_valid_o,
-  input  logic                      data_rx_ready_i,
+  output logic [ADC_NUM_CHS-1:0]               [1:0] data_rx_datasize_o,
+  output logic [ADC_NUM_CHS-1:0]              [31:0] data_rx_o,
+  output logic [ADC_NUM_CHS-1:0]                     data_rx_valid_o,
+  input  logic [ADC_NUM_CHS-1:0]                     data_rx_ready_i,
 
   // ADC signals
   input  logic                      adc_rx_valid_async_i,
@@ -41,17 +44,20 @@ module udma_adc_rx_top #(
 
   logic                 [2:0] adc_data_valid_sync;
   logic  [ADC_DATA_WIDTH-1:0] adc_data_sync;
-
+  logic     [ADC_NUM_CHS-1:0] adc_udma_valid_SP, adc_udma_valid_SN;
 
   logic     adc_vld_edge, adc_vld_edge_del;
-  logic     adc_udma_valid_SP, adc_udma_valid_SN;
+  logic     single_ch_mode;
 
 
   assign adc_rx_valid_sync_o = adc_vld_edge_del;
   assign adc_rx_data_reg_o   = adc_data_sync;
 
   assign data_rx_valid_o     = adc_udma_valid_SP;
-  assign data_rx_o           = {{(32-ADC_DATA_WIDTH){1'b0}}, adc_data_sync};
+
+  always_comb begin
+    for (int i=0; i<ADC_NUM_CHS; i++) data_rx_o[i] = {{(32-ADC_DATA_WIDTH){1'b0}}, adc_data_sync};
+  end
 
   // sync & edge detect of adc_data_valid_i
   always_ff @(posedge sys_clk_i, negedge rst_ni) begin
@@ -75,18 +81,48 @@ module udma_adc_rx_top #(
 
   assign adc_vld_edge = adc_data_valid_sync[1] & ~adc_data_valid_sync[2];
 
-  always_comb begin
-    adc_udma_valid_SN = adc_udma_valid_SP;
-    if (adc_vld_edge)
-      adc_udma_valid_SN = 1'b1;
-    else if (data_rx_ready_i)
-      adc_udma_valid_SN = 1'b0;
-  end
+  generate
+
+    if (ADC_NUM_CHS == 1) begin
+      always_comb begin
+        adc_udma_valid_SN = adc_udma_valid_SP;
+        
+        if (adc_vld_edge)
+          adc_udma_valid_SN[0] = 1'b1;
+        else if (data_rx_ready_i[0])
+          adc_udma_valid_SN[0] = 1'b0;
+      end
+    end
+    else begin
+      always_comb begin
+        adc_udma_valid_SN = adc_udma_valid_SP;
+
+        if (single_ch_mode) begin
+          if (adc_vld_edge)
+            adc_udma_valid_SN[0] = 1'b1;
+          else if (data_rx_ready_i[0])
+            adc_udma_valid_SN[0] = 1'b0;
+        end
+        else begin
+          for (int i=0; i<ADC_NUM_CHS; i++) begin
+            if ( (adc_vld_edge) && (adc_rx_data_i[CH_ID_LSB+CH_ID_WIDTH-1:CH_ID_LSB] == i) )
+              adc_udma_valid_SN[i] = 1'b1;
+            else if (data_rx_ready_i[i])
+              adc_udma_valid_SN[i] = 1'b0;
+          end
+        end
+
+      end
+
+    end
+
+  endgenerate
 
 
-  udma_adc_reg_if #(
+  udma_adc_rx_reg_if #(
     .L2_AWIDTH_NOAL ( L2_AWIDTH_NOAL ),
-    .TRANS_SIZE     ( TRANS_SIZE     )
+    .TRANS_SIZE     ( TRANS_SIZE     ),
+    .ADC_NUM_CHS    ( ADC_NUM_CHS    )
   ) udma_adc_rx_reg_if_i
   (
     .clk_i               ( sys_clk_i            ),
@@ -108,7 +144,9 @@ module udma_adc_rx_top #(
     .cfg_rx_en_i         ( cfg_rx_en_i          ),
     .cfg_rx_pending_i    ( cfg_rx_pending_i     ),
     .cfg_rx_curr_addr_i  ( cfg_rx_curr_addr_i   ),
-    .cfg_rx_bytes_left_i ( cfg_rx_bytes_left_i  )
+    .cfg_rx_bytes_left_i ( cfg_rx_bytes_left_i  ),
+
+    .cfg_single_ch_mode_o( single_ch_mode       )
   );
 
 
